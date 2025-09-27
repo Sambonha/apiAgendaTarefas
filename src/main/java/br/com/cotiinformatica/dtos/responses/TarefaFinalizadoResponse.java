47e01c5a8fbb48fdf4adc578b39c4ec0cb696b05
Sambonha <felipe152016@gmail.com>
2025-09-27 11:59:37 -0300

    Projeto já finalizado

---
+++ src/main/java/br/com/cotiinformatica/dtos/responses/TarefaFinalizadoResponse.java
@@ -0,0 +1,11 @@
+package br.com.cotiinformatica.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TarefaFinalizadoResponse {
+	private String status;
+	private Integer quantidade;
+}
---
+++ src/main/java/br/com/cotiinformatica/dtos/responses/TarefaPrioridadeResponse.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TarefaPrioridadeResponse {
+	
+	private String prioridade;
+	private Integer quantidade;
+
+}
